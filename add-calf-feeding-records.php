3349a931bf5dc38daac8f3e96c92d6b32503aff7
Golola Charles <golola.charles18@gmail.com>
2019-01-25 12:44:52 -0800

    Feeding Module

---
+++ add-calf-feeding-records.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include 'head.php';
+    $active='feeding';
+    ?>
+</head>
+<?php
+include 'db.php';
+$message="";
+$farm =$_SESSION['farm'];
+if(isset($_POST['submit'])){
+    $date = mysqli_real_escape_string($con,  ucwords($_POST['sdate']));
+    $animal_id = mysqli_real_escape_string($con,   ucwords($_POST['animal_id']));
+    $colostrum = mysqli_real_escape_string($con,   ucwords($_POST['colostrum']));
+    $milk = mysqli_real_escape_string($con,   ucwords($_POST['milk']));
+    $supplement = mysqli_real_escape_string($con,   ucwords($_POST['supplement']));
+    $recdate = date("Y-m-d");
+    $recby = $_SESSION['full_names'];
+
+    //capturing the registrar of the data
+    $entered_by =   $_SESSION['full_names'];
+    $time =         date("Y-m-d H:i:s");
+    $action =       "Entered Calf Feeding records for".$tagno;
+
+
+
+    //checking if the member was already paid
+    if(mysqli_num_rows(mysqli_query($con,"select * from calf_feeding where animal_id='$animal_id' and fdate='$date' and farm_id ='$farm'"))>0){
+        //  $message = "<div class=\"alert alert-danger\"><strong>This Calf's Feeding Records were already Taken</strong></div>";
+        echo "<script>alert('This Record were already Taken');</script>";
+    }else{
+        $insert_record = mysqli_query($con,"INSERT INTO calf_feeding (farm_id,animal_id,fdate,colostrumgiven,milkgiven,supplementgiven,recby,recdate)
+                                      VALUES ('$farm','$animal_id', '$date', '$colostrum', '$milk', '$supplement','$recby','$recdate');");
+
+        $insert_log = mysqli_query($con,"insert into transaction_logs(farm_id,transaction_action,transaction_time,transaction_by) VALUES ('$farm','$action','$time','$entered_by')");
+
+        if($insert_record && $insert_log){
+            echo "<script>alert('Recorded is Successfully');</script>";
+        }else{
+            echo mysqli_error($con);
+        }
+    }
+
+}
+
+?>
+<body class="fix-header">
+<!-- ============================================================== -->
+<!-- Preloader -->
+<!-- ============================================================== -->
+<div class="preloader">
+    <svg class="circular" viewBox="25 25 50 50">
+        <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" />
+    </svg>
+</div>
+<!-- ============================================================== -->
+<!-- Wrapper -->
+<!-- ============================================================== -->
+<div id="wrapper">
+    <!-- ============================================================== -->
+    <!-- Topbar header - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    <nav class="navbar navbar-default navbar-static-top m-b-0">
+        <?php include 'nav.php';?>
+    </nav>
+    <!-- End Top Navigation -->
+    <!-- ============================================================== -->
+    <!-- Left Sidebar - style you can find in sidebar.scss  -->
+    <!-- ============================================================== -->
+    <div class="navbar-default sidebar" role="navigation">
+        <?php include 'sidebar.php';?>
+    </div>
+    <!-- ============================================================== -->
+    <!-- End Left Sidebar -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- Page Content -->
+    <!-- ============================================================== -->
+    <div id="page-wrapper">
+        <div class="container-fluid">
+            <div class="row bg-title">
+                <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                    <h4 class="page-title">
+                        Calf Feeding Form
+                    </h4> </div>
+                <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
+                    <button class="right-side-toggle waves-effect waves-light btn-info btn-circle pull-right m-l-20"><i class="ti-settings text-white"></i></button>
+                    <a href="javascript: void(0);" target="_blank" class="btn btn-danger pull-right m-l-20 hidden-xs hidden-sm waves-effect waves-light">Buy Admin Now</a>
+                    <ol class="breadcrumb">
+                        <li><a href="#">Dashboard</a></li>
+                        <li><a href="#">Feeding</a></li>
+                        <li><a href="#">  Calf Feeding Form</a></li>
+                    </ol>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!--.row-->
+            <div class="row">
+
+                <div class="col-md-9 col-sm-12">
+                    <div class="white-box">
+                        <h3 class="box-title m-b-0">  Calf Feeding Form</h3>
+                        <form action="add-calf-feeding-records" method="post" enctype="multipart/form-data">
+                            <div class="row">
+                                <div class="col-md-1"></div>
+                                <div class="col-sm-10 col-xs-12">
+                                    <div class="form-group">
+                                        <label for="exampleInputpwd2">Feeding Date</label>
+                                        <div class="input-group">
+                                            <input type="date" class="form-control" name="sdate" id="datepicker" />
+                                            <span class="input-group-addon"><i class="icon-calender"></i></span>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputuname">Animal</label>
+                                        <div class="input-group">
+                                            <select class="form-control select2" name="animal_id" required>
+                                                <option>Select</option>
+                                                <?php
+                                                $select_tag = mysqli_query($con,"select * from animal_registration where status ='Present' and farm_id ='$farm'");
+                                                while ($tag = mysqli_fetch_array($select_tag)){
+                                                    ?>
+                                                    <option value="<?php echo $tag['animal_id']; ?>"><?php echo $tag['animal_name'].
+                                                            '('.$tag['tagNo'].')'; ?></option>
+                                                    <?php
+                                                }
+                                                ?>
+
+                                            </select>
+                                            <?php
+                                            ?>
+                                            <div class="input-group-addon"><i class="ti-pencil"></i></div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Colostrum  Given</label>
+                                        <div class="input-group">
+                                            <input class="form-control" name="colostrum" required autocomplete="off" placeholder="Quantity of Colostrum Given" type="number">
+                                            <div class="input-group-addon"><i class="ti-pencil-alt"></i></div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Milk  Given</label>
+                                        <div class="input-group">
+                                            <input class="form-control" name="milk" required autocomplete="off" placeholder="Quantity of Milk Given" type="number">
+                                            <div class="input-group-addon"><i class="ti-pencil-alt"></i></div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Supplement  Given</label>
+                                        <div class="input-group">
+                                            <input class="form-control" name="supplement" required autocomplete="off" placeholder="Supplement Provided" type="text">
+                                            <div class="input-group-addon"><i class="ti-pencil-alt"></i></div>
+                                        </div>
+                                    </div>
+
+                                    <div class="text-center">
+                                        <button type="submit" name="submit" class="btn btn-success waves-effect waves-light m-r-10">Submit</button>
+                                    </div>
+                                </div>
+                                <div class="col-md-1"></div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-12">
+                    <h4><b>Calf Feeding Tips</b></h4>
+
+
+                </div>
+            </div>
+            <!-- ============================================================== -->
+        </div>
+        <!-- /.container-fluid -->
+
+        <?php include 'footer.php';?>
+    </div>
+    <!-- /#page-wrapper -->
+</div>
+<!-- /#wrapper -->
+<!-- jQuery -->
+<script src="plugins/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- Menu Plugin JavaScript -->
+<script src="plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.js"></script>
+<!--slimscroll JavaScript -->
+<script src="js/jquery.slimscroll.js"></script>
+<!--Wave Effects -->
+<script src="js/waves.js"></script>
+<!-- Custom Theme JavaScript -->
+<script src="js/custom.min.js"></script>
+
+<script src="js/jasny-bootstrap.js"></script>
+<script src="plugins/bower_components/switchery/dist/switchery.min.js"></script>
+<script src="plugins/bower_components/custom-select/dist/js/select2.full.min.js" type="text/javascript"></script>
+<script>
+    $(function() {
+        // Switchery
+        var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
+        $('.js-switch').each(function() {
+            new Switchery($(this)[0], $(this).data());
+        });
+        // For select 2
+        $(".select2").select2();
+        $('.selectpicker').selectpicker();
+        //Bootstrap-TouchSpin
+        $(".vertical-spin").TouchSpin({
+            verticalbuttons: true
+        });
+        var vspinTrue = $(".vertical-spin").TouchSpin({
+            verticalbuttons: true
+        });
+        if (vspinTrue) {
+            $('.vertical-spin').prev('.bootstrap-touchspin-prefix').remove();
+        }
+        $("input[name='tch1']").TouchSpin({
+            min: 0,
+            max: 100,
+            step: 0.1,
+            decimals: 2,
+            boostat: 5,
+            maxboostedstep: 10,
+            postfix: '%'
+        });
+        $("input[name='tch2']").TouchSpin({
+            min: -1000000000,
+            max: 1000000000,
+            stepinterval: 50,
+            maxboostedstep: 10000000,
+            prefix: '$'
+        });
+        $("input[name='tch3']").TouchSpin();
+        $("input[name='tch3_22']").TouchSpin({
+            initval: 40
+        });
+        $("input[name='tch5']").TouchSpin({
+            prefix: "pre",
+            postfix: "post"
+        });
+        // For multiselect
+        $('#pre-selected-options').multiSelect();
+        $('#optgroup').multiSelect({
+            selectableOptgroup: true
+        });
+        $('#public-methods').multiSelect();
+        $('#select-all').click(function() {
+            $('#public-methods').multiSelect('select_all');
+            return false;
+        });
+        $('#deselect-all').click(function() {
+            $('#public-methods').multiSelect('deselect_all');
+            return false;
+        });
+        $('#refresh').on('click', function() {
+            $('#public-methods').multiSelect('refresh');
+            return false;
+        });
+        $('#add-option').on('click', function() {
+            $('#public-methods').multiSelect('addOption', {
+                value: 42,
+                text: 'test 42',
+                index: 0
+            });
+            return false;
+        });
+    });
+</script>
+<!--Style Switcher -->
+<script src="plugins/bower_components/styleswitcher/jQuery.style.switcher.js"></script>
+</body>
+
+</html>
